b1b74384b9875cee0913a2306cba6ce50f8fe0bc
hoangnvit <van-hoang.nguyen@mohawkcollege.ca>
2021-10-14 03:39:17 -0400

    aaa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'fname'=>'anthony',
+            'lname'=>'nguyen',
+            'email' => 'hoangcnt48@gmail.com',
+            'password' => '$2y$10$j2VtWepcxAi9.LhK8/x8gentVCE/oWUv7iGxiIr3X2FhEAG0AfsCK',
+            'address'=>'103-45 fennell ave East',
+            'postalcode'=>'L9A1R7',
+            'avatar'=>'avatar_default_1628460128.png',
+            'admin'=>1,
+            'type_id'=>0,
+            
+        ]);
     }
 }
