cc17e61c3086d67977271104b68c6094bba17584
hoangnvit <van-hoang.nguyen@mohawkcollege.ca>
2021-10-14 03:48:57 -0400

    aaa

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel='stylesheet' href="{{asset('css/mystyle.css') }}">
+    <link rel='stylesheet' href="{{secure_asset('css/mystyle.css') }}">
 
     @yield('meta')
     <title>@yield('title')</title>
