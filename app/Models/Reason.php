c6450740a85c90fe9055ee1c2a4e19142762a3cc
hoangnvit <van-hoang.nguyen@mohawkcollege.ca>
2021-10-31 06:17:06 -0400

    aaa

--- app/Models/Reason.php
+++ app/Models/Reason.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Reason extends Model
 {
     use HasFactory;
+    public $timestamps = false;
     protected $fillable = ['reason', 'del_num','dis_num'];
 }
