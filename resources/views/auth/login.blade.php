94356b3b7450cbf47266b3929b4eabf1a4249118
hoangnvit <van-hoang.nguyen@mohawkcollege.ca>
2021-11-06 05:00:37 -0400

    aaa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
 
 </div>
 
-<div class='d-flex d-flex justify-content-center border border-2 rounded border-warning p-2'>
+<div class='d-flex d-flex justify-content-center border border-2 rounded '>
     <!-- Session Status -->
     <!-- <x-auth-session-status class="mb-4" :status="session('status')" /> -->
 
