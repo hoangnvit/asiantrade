de17f5f19d2e62b7c99eec5968d883adc05f9984
hoangnvit <van-hoang.nguyen@mohawkcollege.ca>
2021-11-11 15:51:37 -0500

    aaa

--- resources/views/admin/post_delete.blade.php
+++ resources/views/admin/post_delete.blade.php
@@ -16,11 +16,12 @@
 
     <!-- Validation Errors -->
     <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <h4 class="text-warning">You are deleting post: "{{$post['title']}}"</h4>
 
     <form method="POST" action="{{route('admin_posts_delete',['user_id'=>$post['user_id'], 'post_id'=>$post['id']])}}" enctype="multipart/form-data" class='border border-1 p-5 rounded mx-2'>
         @csrf
         <!-- <input id="id" class="block mt-1 w-full" type="hidden" name="id" value="{{ $post_detail['id'] ?? '' }}" required autofocus /> -->
-        <h4>You are deleting post: "{{$post['title']}}"</h4>
+        
         <div class="row mt-2">
             <x-label class="col-4" for="reason_id" :value="__('Reason')" />
 
