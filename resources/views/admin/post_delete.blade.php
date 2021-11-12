e8567782bcf29cdcace185ee8e0d9b23987e819b
hoangnvit <van-hoang.nguyen@mohawkcollege.ca>
2021-11-11 20:03:46 -0500

    aaa

--- resources/views/admin/post_delete.blade.php
+++ resources/views/admin/post_delete.blade.php
@@ -28,8 +28,8 @@
         <!-- <input id="id" class="block mt-1 w-full" type="hidden" name="id" value="{{ $post_detail['id'] ?? '' }}" required autofocus /> -->
         
         <div class="row mt-2">
-            <x-label class="col-4" for="reason_id" :value="__('Reason')" />
-            <p>Choose a reason:</p>
+            <x-label class="col-4" for="reason_id" :value="__(' Choose a Reason')" />
+           
             <select class="col-8" id="reason_id" name="reason_id">
                 @foreach($reasons as $r)
                 <option value="{{$r['id']}}">{{$r['reason']}}</option>
